64870805a6ec1240822477f87c6d857179dfbf52
Pat Dineen <patrickjdineen@gmail.com>
2025-10-17 13:59:27 -0500

    removed alerts

--- frontend/src/Checkout.jsx
+++ frontend/src/Checkout.jsx
@@ -11,94 +11,80 @@ function Checkout({ cartItems, onBack }) {
   //used in get session request and then to create the drop in. 
   const dropinRef = useRef(null)
 
-    //use data from the cart page to hit /post/session on backend.
-
-    const getSession = async () => {
-    alert('Creating a payment session with Adyen');
-
+  //use data from the cart page to hit /post/session on backend.
+  const getSession = async () => {
     let data, adyenGlobalConfig;
     const sessionRequestData = {
         amount: {
             value : adyenTotal,
-
-
             currency : 'USD'
         },
         returnUrl : 'http://localhost:3000'
     };
-    
-    try {
-        const response = await fetch (`${apiUrl}/api/session`, {
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(sessionRequestData)
-        });
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-            }
-        data = await response.json()
-        if(data){
-          alert(` Session created succesfully! \n\n {\n    id : ${data.id},\n    sessionData : ${data.sessionData}\n}`)
-        }
-        
-    } catch (error) {
-        console.error('Error is: ',error)
-    };
+  
+  try {
+      const response = await fetch (`${apiUrl}/api/session`, {
+          method : 'POST',
+          headers : {
+              'Content-Type' : 'application/json'
+          },
+          body : JSON.stringify(sessionRequestData)
+      });
+      if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+          }
+      data = await response.json()
+  } catch (error) {
+      console.error('Error is: ',error)
+  };
 
-    //all config objects for dropin
-    /*
-    const cardConfiguration = {
-        hasHolderName: true, // Show the cardholder name field.
-        holderNameRequired: true, // Mark the cardholder name field as required.
-        billingAddressRequired: true // Show the billing address input fields and mark them as required.
-        };
+  //all config objects for dropin
+  /*
+  const cardConfiguration = {
+      hasHolderName: true, // Show the cardholder name field.
+      holderNameRequired: true, // Mark the cardholder name field as required.
+      billingAddressRequired: true // Show the billing address input fields and mark them as required.
+      };
 */
     const dropinConfiguration = {
-        // Required if you import individual payment methods.
-        paymentMethodComponents: [Card, GooglePay, PayPal, ApplePay, Klarna, Ach],
-        openFirstPaymentMethod : true,
-        showPaymentMethods : true,
+      // Required if you import individual payment methods.
+      paymentMethodComponents: [Card, GooglePay, PayPal, ApplePay, Klarna, Ach],
+      openFirstPaymentMethod : true,
+      showPaymentMethods : true,
 
-        // Optional configuration.
-        //onReady: () => {},
-        instantPaymentTypes: ['applepay', 'googlepay']
-        };
+      // Optional configuration.
+      //onReady: () => {},
+      instantPaymentTypes: ['applepay', 'googlepay']
+      };
 
-        adyenGlobalConfig = {
-            session : {
-                id : data.id,
-                sessionData : data.sessionData
-            },
-            amount : sessionRequestData.amount,
-            environment : 'TEST',
-            countryCode : 'US',
-            locale : 'en-US',
-            clientKey : data.clientKey,
-          //event handlers for payment responses
-            onPaymentCompleted: (result, component) => {
-              //alert(`Payment completed successfully with code ${result.resultCode}`)
-                console.info(result, component);
-            },
-            onPaymentFailed: (result, component) => {
-              alert(`Payment failed with code ${result.resultCode}`)
-                console.info(result, component);
-            },
-            onError: (error, component) => {
-                console.error(error.name, error.message, error.stack, component);
-            }
-        };
-
-        //create and mount dropin container using above objects
-        const checkout = await AdyenCheckout(adyenGlobalConfig);
-        if(checkout){
-          alert('Now creating Drop-in')
-        }
-        const drop = new Dropin(checkout,dropinConfiguration).mount('#dropin-container');
+    adyenGlobalConfig = {
+      session : {
+          id : data.id,
+          sessionData : data.sessionData
+      },
+      amount : sessionRequestData.amount,
+      environment : 'TEST',
+      countryCode : 'US',
+      locale : 'en-US',
+      clientKey : data.clientKey,
+    //event handlers for payment responses
+      onPaymentCompleted: (result, component) => {
+          console.info(result, component);
+      },
+      onPaymentFailed: (result, component) => {
+          console.info(result, component);
+      },
+      onError: (error, component) => {
+          console.error(error.name, error.message, error.stack, component);
+      }
     };
 
-    getSession();
+  //create and mount dropin container using above objects
+  const checkout = await AdyenCheckout(adyenGlobalConfig);
+  const drop = new Dropin(checkout,dropinConfiguration).mount('#dropin-container');
+  };
+
+  getSession();
      
   return (
     <div className="checkout-page">
